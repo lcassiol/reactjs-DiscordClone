1d1c7c9a42adf167aed599485da9d73cd78b6be5
Cassio C <LccassioL@gmail.com>
2020-07-28 17:57:22 -0300

    Organize folders and add eslint and prettier

---
+++ src/components/ChannelButton/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ChannelButton: React.FC = () => {
+  return <Container></Container>;
+};
+
+export default ChannelButton;
