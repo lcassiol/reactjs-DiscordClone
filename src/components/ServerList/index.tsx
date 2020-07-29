7d4ebdf6eaa904f22f3166533278fb64d2215f26
Cassio C <LccassioL@gmail.com>
2020-07-29 16:16:42 -0300

    Implement serverlist

--- src/components/ServerList/index.tsx
+++ src/components/ServerList/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Separator } from './styles';
+import ServerButton from '../ServerButton';
 
 const ServerList: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={12} />
+      <ServerButton />
+      <ServerButton selected />
+    </Container>
+  );
 };
 
 export default ServerList;
